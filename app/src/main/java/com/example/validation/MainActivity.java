343213aff10271e4c9fcf6bd8b79e5954587feaf
Ucchwas <cpslabttu@gmail.com>
2024-04-02 16:13:14 -0500

    Updated

--- app/src/main/java/com/example/validation/MainActivity.java
+++ app/src/main/java/com/example/validation/MainActivity.java
@@ -4,12 +4,17 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.os.Environment;
+import android.util.Log;
 
 import com.example.validation.ml.AnnClassifier;
 import com.example.validation.ml.AnnMulticlass;
+import com.example.validation.ml.Apnea;
 import com.example.validation.ml.ArrhythmiaOnEcgClassification;
 import com.example.validation.ml.CnnMulticlass;
+import com.example.validation.ml.HybridMulticlass;
+import com.example.validation.ml.LstmMulticlassNew;
 import com.example.validation.ml.RnnLstmMulticlass;
+import com.example.validation.ml.RnnSimpleMulticlass;
 import com.opencsv.CSVReader;
 import com.opencsv.CSVWriter;
 
@@ -25,21 +30,28 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.Date;
 import java.util.List;
+import java.util.Locale;
 import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.stream.Collectors;
 
 public class MainActivity extends AppCompatActivity {
+    long startTimeMillis  = System.currentTimeMillis();
+    Date currentDate = new Date(startTimeMillis);
+    SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss.SSS", Locale.getDefault());
+    String formattedDateTime = sdf.format(currentDate);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         int k = 0;
-        int[] predictClassCount = new int[5];
+        int[] predictClassCount = new int[2];
         String folderPath = Environment.getExternalStorageDirectory().getAbsolutePath() + "/Data ";
         CopyOnWriteArrayList<String[]> data = new CopyOnWriteArrayList<>();
         CSVWriter writer = null;
@@ -56,7 +68,7 @@ public class MainActivity extends AppCompatActivity {
             }
         }
 
-        try (InputStream inputStream = getResources().openRawResource(R.raw.mitbih_test);
+        try (InputStream inputStream = getResources().openRawResource(R.raw.features_test);
              InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
              BufferedReader reader = new BufferedReader(inputStreamReader);
              CSVReader csvReader = new CSVReader(reader)) {
@@ -75,10 +87,10 @@ public class MainActivity extends AppCompatActivity {
                     writer = new CSVWriter(new FileWriter(fileECG, true));
                     try {
                         int predictClass = 0;
-                        AnnMulticlass model = AnnMulticlass.newInstance(this);
+                        Apnea model = Apnea.newInstance(this);
 
                         // Creates inputs for reference.
-                        TensorBuffer inputFeature0 = TensorBuffer.createFixedSize(new int[]{1, 186, 1}, DataType.FLOAT32);
+                        TensorBuffer inputFeature0 = TensorBuffer.createFixedSize(new int[]{1, 40}, DataType.FLOAT32);
 
 
                         // Find the minimum and maximum values of the ECG data
@@ -94,7 +106,7 @@ public class MainActivity extends AppCompatActivity {
                         }
 
                         // Pack normalized ECG data into a ByteBuffer
-                        ByteBuffer byteBuffer = ByteBuffer.allocate(186 * 4);
+                        ByteBuffer byteBuffer = ByteBuffer.allocate(40 * 4);
                         byteBuffer.order(ByteOrder.LITTLE_ENDIAN);
                         for (Double sample : justEcg) {
                             float samplefloat = (float) ((sample - ecgMin) / (ecgMax - ecgMin));
@@ -108,14 +120,15 @@ public class MainActivity extends AppCompatActivity {
                         inputFeature0.loadBuffer(byteBuffer);
 
                         // Runs model inference and gets result.
-                        AnnMulticlass.Outputs outputs = model.process(inputFeature0);
+                        Apnea.Outputs outputs = model.process(inputFeature0);
                         TensorBuffer outputFeature0 = outputs.getOutputFeature0AsTensorBuffer();
 
                         // Get predicted class
                         float[] scores = outputFeature0.getFloatArray();
-//                    for (int j=0;j<scores.length;j++){
-//                        System.out.println("Scores : " + scores[j]);
-//                    }
+                        for(float score : scores){
+                            System.out.println(score);
+                        }
+
                         predictClass = getMaxIndex(scores);
                         //data.add(new String[]{String.valueOf(k), String.valueOf(predictClass)});
 
@@ -124,6 +137,7 @@ public class MainActivity extends AppCompatActivity {
 
                         // Releases model resources if no longer used.
                         model.close();
+                        calculateAndLogElapsedTime();
                         k++;
                         writer.writeAll(Collections.singleton(new String[]{String.valueOf(k), String.valueOf(predictClass)}));
                     } catch (IOException e) {
@@ -166,5 +180,19 @@ public class MainActivity extends AppCompatActivity {
                 return 0;
         }
         return 1;
+    }
+
+    private void calculateAndLogElapsedTime() {
+        long currentTimeMillis = System.currentTimeMillis();
+
+        long elapsedTimeMillis = currentTimeMillis - startTimeMillis;
+
+        Date elapsedTimeDate = new Date(elapsedTimeMillis);
+
+        SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss.SSS", Locale.getDefault());
+
+        String formattedElapsedTime = sdf.format(elapsedTimeDate);
+
+        //Log.d("ElapsedTime", "Elapsed Time: " + formattedElapsedTime);
     }
 }
